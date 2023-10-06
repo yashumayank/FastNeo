995e4dfe31ccc59cbbd342f7afbd6fe7424f89a2
Mayank Mahajan <yashumayank@hotmail.com>
2023-10-06 16:45:33 -0400

    Update runEpitopeDB_SNV_neoepitopes.sh
    Signed-off-by: Mayank Mahajan <yashumayank@hotmail.com>

--- runEpitopeDB_SNV_neoepitopes.sh
+++ runEpitopeDB_SNV_neoepitopes.sh
@@ -14,7 +14,7 @@ wait
 awk -v pid=${sample}_1 '{if(FNR==1){f++};if(f<=2){if(FNR%4==1){a[$1]=1}}else{if(FNR%4==1){if(a[$1]==1){x=1}else{x=0}};if(x==1){print $0 > pid ".nullomers.union.epitopeDB.fastq"}}}' ${sample}_*.nullomers_epitopeDB.fastq ${sample}_1.fastq &
 awk -v pid=${sample}_2 '{if(FNR==1){f++};if(f<=2){if(FNR%4==1){a[$1]=1}}else{if(FNR%4==1){if(a[$1]==1){x=1}else{x=0}};if(x==1){print $0 > pid ".nullomers.union.epitopeDB.fastq"}}}' ${sample}_*.nullomers_epitopeDB.fastq ${sample}_2.fastq
 
-rm ${sample}_1.fastq ${sample}_2.fastq
+#rm ${sample}_1.fastq ${sample}_2.fastq
 rm ${sample}_*.nullomers_epitopeDB.fastq ${sample}_*.nullomers_ChimerDB.fastq ${sample}_*.nullomers_TE.fastq
 rm ${sample}_1.nullomers.json ${sample}_1.nullomers.json
 wait
@@ -30,7 +30,6 @@ rm test test1 test2 ${sample}.nullomers.union.epitopeDB.sam
 #find coverage for each nullomer correspoding to specific neoepitope ; apply read length filter >60 bp
 samtools view ${sample}.nullomers.union.epitopeDB.bam | awk -v "sid=${sample}" -F "\t" '{if(FNR==1){fn++; split(FILENAME,dbtmp,"/");split(dbtmp[length(dbtmp)],dbName,"_n")};if(fn<=3){if($1!="" || NF==5){split($1,u,";");if(null_list[$4]==""){ann[$4]=$5;null_list[$4]=$1;for(i=1;i<=length(u);i++){null_neo[$4"_"u[i]]=$3;null_db[$4"_"u[i]]=dbName[1]}}else{for(i=1;i<=length(u);i++){if(index(null_list[$4],u[i])==0){null_list[$4]=null_list[$4]";"u[i]};if(null_neo[$4"_"u[i]]==""){null_neo[$4"_"u[i]]=$3}else{if(index(null_neo[$4"_"u[i]],$3)==0){null_neo[$4"_"u[i]]=null_neo[$4"_"u[i]]";"$3}};if(null_db[$4"_"u[i]]==""){null_db[$4"_"u[i]]=dbName[1]}else{if(index(null_db[$4"_"u[i]],dbName[1])==0){null_db[$4"_"u[i]]=null_db[$4"_"u[i]]";"dbName[1]}}}}}}else{if(length($10)>60){split($3,v,".");if(et!=v[1]){et=v[1];nt=split(null_list[et],nlu,";")};for(i=1;i<=length(nlu);i++){if(index($10,nlu[i])>0){null_count[et"_"nlu[i]]++}}}}}END{for(k in null_count){split(k,w,"_");print sid"\t"w[2]"\t"null_neo[k]"\t"null_count[k]"\t"w[1]"\t"null_db[k]"\t"ann[w[1]]}}' ${pathDB}IEDB_neoepitopes-nullomers.filtered.tsv ${pathDB}TSNAdb_TCGA_neoepitopes-nullomers.filtered.tsv ${pathDB}TSNAdb_ICGC_neoepitopes-nullomers.filtered.tsv  - > ${sample}_epitopeDB_neoepitopes_counts_raw.tsv
 
-#bedtools
 #Calculate coverage of the each neoepitope as the coverage of most covered corresponding nullomer; read coverage and nullomers discovered on the read are reported
 sort -k1,1 -k5,5 -k3,3 -k4n -t$'\t' ${sample}_epitopeDB_neoepitopes_counts_raw.tsv | awk -F "\t" 'BEGIN{print "sample_id\tENST_id\ttop_nullomer\tneoepitopes\t#reads\t#nullomers\tdb_name\tannotation"}{if(neo_list!=$3 || et!=$5){if(FNR!=1 && rcount>1){print pid"\t"et"\t"null"\t"neo_list"\t"rcount"\t"ncount"\t"dbName"\t"ann}; pid=$1; neo_list=$3; et=$5; dbName=$6; ann=$7; ncount=0}; ncount++;rcount=$4; null=$2}END{if(rcount>1){print pid"\t"et"\t"null"\t"neo_list"\t"rcount"\t"ncount"\t"dbName"\t"ann}}' > ${sample}_epitopeDB_neoepitopes_readCounts.tsv
 rm ${sample}.nullomers.union.epitopeDB.bam
