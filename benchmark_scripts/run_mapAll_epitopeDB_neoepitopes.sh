ffc9de370d26165087a4fe9535bd0510f9c56b11
Mayank Mahajan <yashumayank@hotmail.com>
2024-03-11 14:16:52 -0400

    Update run_mapAll_epitopeDB_neoepitopes.sh
    Signed-off-by: Mayank Mahajan <yashumayank@hotmail.com>

--- benchmark_scripts/run_mapAll_epitopeDB_neoepitopes.sh
+++ benchmark_scripts/run_mapAll_epitopeDB_neoepitopes.sh
@@ -9,7 +9,10 @@ sample=$1
 #julia
 #/data/hemberg/shared_resources/sratoolkit.2.11.2-centos_linux64/bin/fasterq-dump --split-3 ${sample}
 
-#bowtie2
+stSTAR=$(date +%s)
+cspeed=$(lscpu|grep 'Model name'|awk -F ":" '{gsub(/^ */,"",$2);print $2}')
+
+module load bowtie2
 bowtie2 -x ${pathMI}Homo_sapiens.GRCh38.cds.all -p $cores  --no-unal --omit-sec-seq --local -1 ${sample}_1.fastq -2 ${sample}_2.fastq -S ${sample}.all.sam
 rm ${sample}_1.fastq ${sample}_2.fastq
 
